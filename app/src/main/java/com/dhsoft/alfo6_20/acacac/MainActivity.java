193c6497b06c88ad38a403adcc47a1ab5a85f455
dh <dh@naver.com>
2018-04-26 15:17:50 +0900

    Initial commit

---
+++ app/src/main/java/com/dhsoft/alfo6_20/acacac/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dhsoft.alfo6_20.acacac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
